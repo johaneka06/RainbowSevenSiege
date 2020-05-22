ca4c3dfa6138c8c0474ba211ac3f42b3c16da6fd
AlbertZasura <albertalbert841@gmail.com>
2020-05-23 05:31:28 +0700

    Fifth Commit

---
+++ script/HomeScript.js
@@ -0,0 +1,20 @@
+$(function(){
+    
+    // Disini mulai slider image
+
+    var $slider = $('.slider')
+    var $slideImages = $slider.find('.slideImages')
+    var $images = $slideImages.find('.image')
+    var currSlide = 1
+
+    // Operasi slide
+    setInterval(function(){
+        $slideImages.animate({'margin-left': '-=1920px'}, 1000, function(){
+            currSlide++
+            if(currSlide === $images.length){
+                currSlide = 1
+                $slideImages.css('margin-left', 0)
+            }
+        })
+    }, 3000)
+})
